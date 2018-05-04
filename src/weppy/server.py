8aac1e08d2470dbd7f8f21854e7474ace1a56fbd
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2018-05-04 08:38:00 +0200

    First Flask app

--- src/weppy/server.py
+++ src/weppy/server.py
@@ -1,2 +1,18 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+
+api.add_resource(HelloWorld, '/')
+
+
 if __name__ == '__main__':
-    print('Hello, world!')
+    app.run(debug=True)
