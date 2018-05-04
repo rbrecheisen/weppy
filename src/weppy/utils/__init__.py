f744a789229b4fd823b37235616b9cfb04b84cf5
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2018-05-04 08:32:18 +0200

    First commit

---
+++ src/weppy/utils/__init__.py
@@ -0,0 +1,36 @@
+import json
+
+
+class ArgParser(object):
+
+    def __init__(self):
+        self.args = {}
+        self.args_mandatory = []
+
+    def add_arg(self, name, optional=False):
+        if name.startswith('--'):
+            name = name[2:]
+        self.args[name] = None
+        if not optional:
+            self.args_mandatory.append(name)
+
+    def parse_args(self, args):
+        if len(args) != len(self.args_mandatory):
+            raise RuntimeError('Number of mandatory arguments does not match')
+        for arg in args:
+            name, value = arg.split('=')[0], arg.split('=')[1]
+            if name.startswith('--'):
+                name = name[2:]
+            if name.startswith('-'):
+                name = name[1:]
+            if name not in self.args.keys():
+                raise RuntimeError('Argument {} not in {}'.format(name, self.args.keys()))
+            if value.startswith('{') and value.endswith('}'):
+                value = json.loads(value)
+            self.args[name] = value
+        return self.args
+
+    @staticmethod
+    def print_args(args, indent=4):
+        print('Arguments:')
+        print(json.dumps(args, indent=indent))
